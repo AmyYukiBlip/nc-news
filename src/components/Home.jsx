389498e25f903d4c2824bdb8dcc6a2386c8e0e13
Amy <amy_hanwell@hotmail.com>
2025-02-18 18:58:44 +0000

    CSS changes to nav bar -> wider, top of page, left links & right coloured button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,8 @@ export default function Home(){
         <div >
             <h1>Northcoders News</h1>
             <h2>Spilling all the tea ☕</h2>
+            <p>coming soon... Latest article</p>
+            <p>Coming soon... Categories</p>
             <Link className="red-button" to="/articles">All Articles</Link>
         </div>
     )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,10 +2,14 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <nav>
-      <Link className="home-link" to="/">
+    <ul className="nav-container">
+      <li className="nav-item-left"><Link className="nav-link-left" to="/">
         Home
-      </Link>
-    </nav>
+      </Link></li>
+      <li className="nav-item-left"><Link className="nav-link-left" to="/articles">
+        Articles
+      </Link></li>
+      <li className="nav-item-right"><p className="nav-link-right">Login</p></li>
+    </ul>
   );
 }
