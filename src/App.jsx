34ffc538b9dfe97da47bd87ebf6ef458c356972f
Amy <amy_hanwell@hotmail.com>
2025-02-20 13:00:57 +0000

    Uses useContext for frontend user login based on get users from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
 import { Routes, Route } from "react-router-dom";
 import { useState } from "react";
-
-import Home from "./components/Home"
+import { UserAccount } from "./components/UserAccount";
+import Home from "./components/Home";
 import AllArticles from "./components/AllArticles";
 import Article from "./components/Article";
 import CommentCard from "./components/CommentCard";
 import NavBar from "./components/NavBar";
+import LoginForm from "./components/LoginForm";
 
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState("");
 
   return (
     <>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/articles" element={<AllArticles />} />
-      <Route path="/articles/:article_id" element={<Article  />} />
-      <Route path="/articles/:article_id/comments" element={<CommentCard />} />
-    </Routes>
+      <UserAccount.Provider value={{loggedInUser, setLoggedInUser}}>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/articles" element={<AllArticles />} />
+          <Route path="/articles/:article_id" element={<Article />} />
+          <Route
+            path="/articles/:article_id/comments"
+            element={<CommentCard />}
+          />
+          <Route path="/login" element={<LoginForm />} />
+        </Routes>
+      </UserAccount.Provider>
     </>
   );
 }
 
 export default App;
-
-
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,28 @@
+import { useContext, React } from "react";
 import { Link } from "react-router-dom";
+import { UserAccount } from "./UserAccount";
 
 export default function NavBar() {
+  const loggedInUser = useContext(UserAccount);
+  console.log("User logged in is:", loggedInUser);
+
   return (
     <ul className="nav-container">
-      <li className="nav-item-left"><Link className="nav-link-left" to="/">
-        Home
-      </Link></li>
-      <li className="nav-item-left"><Link className="nav-link-left" to="/articles">
-        Articles
-      </Link></li>
-      <li className="nav-item-right"><p className="nav-link-right">Login</p></li>
+      <li className="nav-item-left">
+        <Link className="nav-link-left" to="/">
+          Home
+        </Link>
+      </li>
+      <li className="nav-item-left">
+        <Link className="nav-link-left" to="/articles">
+          Articles
+        </Link>
+      </li>
+      <li className="nav-item-right">
+        <Link className="nav-link-right" to="/login">
+          Login
+        </Link>
+      </li>
     </ul>
   );
 }
