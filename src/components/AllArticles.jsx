70752ea5271835bd215c14b218b27c0017e6035b
Amy <amy_hanwell@hotmail.com>
2025-02-21 15:28:36 +0000

    Task 11 - adds sort by buttons for created_at, comment_count & votes + Links topic cards to filtered articles

--- src/components/AllArticles.jsx
+++ src/components/AllArticles.jsx
@@ -8,27 +8,61 @@ export default function AllArticles() {
   const [isLoading, setIsLoading] = useState(true);
   const [searchParams, setSearchParams] = useSearchParams();
   const filterQuery = searchParams.get("topic");
+  const orderQuery = searchParams.get("order");
+  const sortByQuery = searchParams.get("sort_by");
 
+  const allButton = () => {
+    const newParams = new URLSearchParams(searchParams);
+    newParams.delete("topic");
+    newParams.delete("order");
+    newParams.delete("sort_by");
+    setSearchParams(newParams)
+  };
+
+  // ** Filter topics **
   const setFilter = (filter) => {
     const newParams = new URLSearchParams(searchParams);
-    newParams.set("topic", filter);
-    setSearchParams(newParams);
+    if (filter) {
+      newParams.set("topic", filter);
+      setSearchParams(newParams);
+    }
+  };
+
+  //  ** Sort articles by order **
+  const setSortOrder = (direction) => {
+    const newOrderParams = new URLSearchParams(searchParams);
+    if (direction) {
+      newOrderParams.set("order", direction);
+      setSearchParams(newOrderParams);
+    }
+  };
+
+  //  ** Sort articles by greenlisted sort queries **
+  const setSortBy = (sort) => {
+    const newSortParams = new URLSearchParams(searchParams);
+    if (sort) {
+      newSortParams.set("sort_by", sort);
+      setSearchParams(newSortParams);
+    }
   };
 
   useEffect(() => {
     setIsLoading(true);
     ncNewsApi
-      .get("/articles", { params: { topic: filterQuery } })
+      .get("/articles", {
+        params: { topic: filterQuery, sort_by: sortByQuery, order: orderQuery },
+      })
       .then((res) => {
         setArticles(res.data.articles);
         setIsLoading(false);
       })
       .catch((err) => {
         setIsLoading(false);
+        console.log(err);
       });
-  }, [filterQuery]);
+  }, [filterQuery, sortByQuery, orderQuery]);
 
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading) return <p>Fetching articles...</p>;
 
   return (
     <>
@@ -37,7 +71,7 @@ export default function AllArticles() {
         Grab a cuppa, and read more news below...
       </h2>
       <p>View all or filter by topic 👇 </p>
-      <button className="red-button" onClick={() => setFilter("")}>
+      <button className="red-button" onClick={() => allButton()}>
         All
       </button>
       <button className="red-button" onClick={() => setFilter("football")}>
@@ -49,12 +83,42 @@ export default function AllArticles() {
       <button className="red-button" onClick={() => setFilter("cooking")}>
         Cooking
       </button>
-      <section className="article-card-row" >
+      <button className="pale-red-button" onClick={() => setSortOrder("desc")}>
+        Newest
+      </button>
+      <button className="pale-red-button" onClick={() => setSortOrder("asc")}>
+        Oldest
+      </button>
+
+      <p>Sort by comments 💬 </p>
+      <button className="red-button" onClick={() => setSortBy("comment_count")}>
+        Comments
+      </button>
+      <button className="pale-red-button" onClick={() => setSortOrder("desc")}>
+        Most
+      </button>
+      <button className="pale-red-button" onClick={() => setSortOrder("asc")}>
+        Least
+      </button>
+      <p>Sort by votes 👍 </p>
+      <button className="red-button" onClick={() => setSortBy("votes")}>
+        Votes
+      </button>
+      <button className="pale-red-button" onClick={() => setSortOrder("desc")}>
+        Most
+      </button>
+      <button className="pale-red-button" onClick={() => setSortOrder("asc")}>
+        Least
+      </button>
+      <br />
+      <br />
+
+      <section className="article-card-row">
         <div className="article-card-column">
           {articles.map((article) => {
-          return <ArticleCard key={article.article_id} article={article} />;
-        })}
-        {/* ^^ mapped {article} object is being passed to ArticleCard */}
+            return <ArticleCard key={article.article_id} article={article} />;
+          })}
+          {/* ^^ mapped {article} object is being passed to ArticleCard */}
         </div>
       </section>
     </>
